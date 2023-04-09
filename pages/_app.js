729977ff2503da4dd9a2ca5a3ad9a1ebb96cabc4
karishma <karishma1997.s@gmail.com>
2023-04-09 11:51:17 +0530

    configure:
    - app.tsx converted to .js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 import {NextPageWithLayout } from '../lib/next/types'
 import "./../pages/customcss.css";
 import "../styles/calendar.css"
 import 'bootstrap/dist/css/bootstrap.css'
-type CustomAppProps = AppProps & {
-	Component: NextPageWithLayout
-}
 
-export default function App({ Component, pageProps }: CustomAppProps) {
-  const getLayout = Component.getLayout ?? ((page: any) => page);
+export default function App({ Component, pageProps }) {
+  const getLayout = Component.getLayout ?? ((page) => page);
 
   return <>{getLayout(<Component {...pageProps} />)}</>;
 }
